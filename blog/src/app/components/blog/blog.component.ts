413b3a17eb96b720c55ca3805f175c2c161819be
Jose Pablo <jpablogpro@hotmail.com>
2024-07-04 19:40:20 +0200

    creacion de array de noticias

--- blog/src/app/components/blog/blog.component.ts
+++ blog/src/app/components/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Inoticia } from '../../interfaces/Inoticia.interface';
 
 @Component({
   selector: 'app-blog',
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
-
+  
+  arrNoticias: Inoticia[] = [
+    {
+      title: 'noticia 1',
+      url: 'url 1',
+      text: 'cuerpo de la noticia 1',
+      date: '2024-07-01'
+    },
+    {
+      title: 'noticia 2',
+      url: 'url 2',
+      text: 'cuerpo de la noticia 2',
+      date: '2024-07-02'
+    }
+  ]
 }
